0a7dfea2aeb8df9253ae9fc769334d98ec5f2c35
Alvino Barboza <alvinobarboza@gmail.com>
2020-07-29 11:21:53 -0300

    config json changed

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@ int main() {
     //drogon::app().addListener("0.0.0.0",8080);
     //drogon::app().setDocumentRoot("/home/alvino/workspace");
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("../../config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
