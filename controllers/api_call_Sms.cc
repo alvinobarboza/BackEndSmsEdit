e00f55a9803f1728bbce59d24f89852b2f4ab8d5
Alvino Barboza <alvinobarboza@gmail.com>
2020-08-06 12:18:19 -0300

    Added token generation for external api

--- controllers/api_call_Sms.cc
+++ controllers/api_call_Sms.cc
@@ -7,34 +7,34 @@ void Sms::getUserSMS(const HttpRequestPtr &req,
                              const std::string &search)const
 {
     LOG_DEBUG << "get SMS";
-    static bool check = true;
+   
     Json::Value json, dataJ, searchJ, wild_search;
+
+    time_t t = time(0);
+    std::string sTime = std::to_string(t);
+    std::string login = "alvino.barboza@youcast.tv.br", secret = "cm3yaca6xr37xp5d0b10vw6d8yhcwd9zk1b7o0be";
+    std::string token =  sha1(sTime+login+secret);
 
     wild_search["wild_search"] = search;
     searchJ["search"] = wild_search;
     dataJ["data"]=searchJ;
-
     
-    //requestJson = dataJ;
+    
+    std::cout << dataJ <<"\n end Json "<<" token: "<< token << std::endl;
 
-    int count = 0;
     auto client = HttpClient::newHttpClient("https://sms.youcast.tv.br");
     auto requestH = HttpRequest::newHttpRequest();
+
     requestH->setMethod(drogon::Post);
     requestH->setPath("/api/customer/findCustomerForSales");
-    requestH->addHeader("Authorization","alvino.barboza@youcast.tv.br:1596657548:9bb9bb1d669c22d5cc4525ca1add9d5ffc6db678");
-  //  requestH->setContentTypeCode(CT_APPLICATION_JSON);
+    requestH->addHeader("Authorization","alvino.barboza@youcast.tv.br:"+sTime+":"+token);
+    requestH->setContentTypeCode(CT_APPLICATION_JSON);
     requestH->newHttpJsonRequest(dataJ);
 
         
     auto a = client->sendRequest(requestH);
 
     json = *a.second->getJsonObject();
-
-    
-    
-    std::cout << dataJ <<" end Json"<< sha1("teste") <<std::endl;
-
     auto resp=HttpResponse::newHttpJsonResponse(json);
     callback(resp);
     
